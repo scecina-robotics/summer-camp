a2f039b339847ad2bb33a18d68825faef1661008
Andy Smith <robotics@Andys-MacBook.local>
2018-06-19 21:52:50 -0400

    First test

--- src/opcontrol.c
+++ src/opcontrol.c
@@ -61,19 +61,34 @@ int RightStickUpDown;
 // Button Switch Toggle
 bool ButtonSwitch;
 
+// DAY 3 - Variables
+// Button to turn on automation
+bool AutoOn;
+// Button to turn off automation
+bool AutoOff;
+// Toggle for automation
+bool AutoToggle;
+// Variable for Automation Turn Speed
+int TurnSpeed;
+int AutoSpeed;
+// Variables for robot to turn
+int Time45;
+int Time90;
+int Time135;
+int Time180;
+// Variable for time to backup
+int TimeBack;
+int TimePause;
+// Variable for Distance to stop.
+int Distance;
+int StopDistance;
 
 void operatorControl() { // Main function for controlling robot
 	// We need to preset the ButtonSwitch before starting loop
 	ButtonSwitch = true;
+	AutoToggle = false;
+	// Use current time as seed for random generator
 	while (1) {
-
-		/* Note: Since while loop is set to
-		 * true (1) it will never end.
-		 * This is called an infinite loop.
-		 * Usually a bad thing, but in this case
-		 * allows program to remain running while
-		 * robot is on.
-		 */
 
 		/*
 		 * Third:
@@ -95,54 +110,126 @@ void operatorControl() { // Main function for controlling robot
 		LeftAdjust = 1; // Allowed values 0 to 1
 		RightAdjust = 1; //Allowed valuers 0 to 1
 
+		// DAY 3 - Variable assignment:
+		//Auto Buttons
+		AutoOn = joystickGetDigital(1, 6, JOY_UP);
+		AutoOff = joystickGetDigital(1, 6, JOY_DOWN);
+		// Speeds
+		TurnSpeed = 50;
+		AutoSpeed = 50;
+		// Variables for robot to turn
+		Time45 = 100;
+		Time90 = 200;
+		Time135 = 300;
+		Time180 = 400;
+		// Variable for time to backup
+		TimeBack = 100;
+		TimePause = 50;
+		// Variable for Distance and stop.
+		Distance = ultrasonicGet(sonar);
+		StopDistance = 10;
+
 		/*
 		 * Fourth:
 		 * ------
 		 * Code robot operations
 		 */
 
-		if(ButtonSwitch){
-			// Base Drive Code
-			if(ForwardButton != 0){
-			 BaseForward(MaxSpeed);
-			}else if(ReverseButton != 0){
-			 BaseReverse(MaxSpeed);
-			}else if(LeftButton != 0){
-			 BaseTurnLeft(MaxSpeed);
-			}else if(RightButton != 0){
-			 BaseTurnRight(MaxSpeed);
-			}else{
-			 LeftWheels(LeftStickUpDown);
-			 RightWheels(RightStickUpDown);
-			}
-	  }else{
-			BaseStop();
-		}
+		 // DAY 3 Code:
 
-
-		// Challenge #1 - Button Stop/Start
-
-		/*
-		 * 1. Add a button toggle variable (bool)
-		 * 2. Code if statements for BUTTON_SWITCH_!, BUTTON_SWITCH_2
-		 * 	  condition = digitalRead(BUTTON_SWITCH_1) == LOW
-		 *    use digitla ports 1 and 2
-		 * 3. Code one button to stop robot control
-		 *		and one button to start robot control
-		 *		Hint: Nested if statement.
-		 * 4. Include print statements to view button status on computer.
-		 * 5. Play Tag.
+		 //Toggle Automation
+		 /*
+		 if(AutoOn != 0){
+			 AutoToggle = true;
+			 print("Auto Toggle On");
+		 }
+		 if(AutoOff != 0){
+			 AutoToggle = false;
+			 print("Auto Toggle Off");
 		 */
+/*
+		 // Automation
+		 while(AutoToggle){
+			 if(Distance > StopDistance){
+				 BaseForward(AutoSpeed);
+			 }else{
+				 BaseStop();
+				 BaseReverse(AutoSpeed);
+				 delay(TimeBack);
+				 BaseStop();
+				 delay(TimePause);
+				 switch (rand() % 7) {
+				 	case 0:
+						BaseTurnLeft(AutoSpeed);
+						delay(Time45);
+						break;
+					case 1:
+						BaseTurnLeft(AutoSpeed);
+						delay(Time90);
+						break;
+					case 2:
+						BaseTurnLeft(AutoSpeed);
+						delay(Time135);
+						break;
+					case 3:
+						BaseTurnLeft(AutoSpeed);
+						delay(Time180);
+						break;
+					case 4:
+						BaseTurnRight(AutoSpeed);
+						delay(Time135);
+						break;
+					case 5:
+						BaseTurnRight(AutoSpeed);
+						delay(Time90);
+						break;
+					case 6:
+						BaseTurnRight(AutoSpeed);
+						delay(Time45);
+						break;
+					default:
+						BaseStop();
+				 }
+				 BaseStop();
+				 delay(TimePause);
+				 BaseForward(AutoSpeed);
+			 }
+			 if(AutoOff != 0){
+				 AutoToggle = false;
+				 print("Auto Toggle Off");
+			 }
+		 }
+*/
+		 //
+		//Disable control is stop button pushed or automation in progress
 
-		 if (digitalRead(BUTTON_SWITCH_1) == LOW ){
+			if(ButtonSwitch){
+				// Base Drive Code
+				if(ForwardButton != 0){
+				 BaseForward(MaxSpeed);
+				}else if(ReverseButton != 0){
+				 BaseReverse(MaxSpeed);
+				}else if(LeftButton != 0){
+				 BaseTurnLeft(MaxSpeed);
+				}else if(RightButton != 0){
+				 BaseTurnRight(MaxSpeed);
+				}else{
+				 LeftWheels(LeftStickUpDown);
+				 RightWheels(RightStickUpDown);
+				}
+		  }else{
+				BaseStop();
+			}
+			// Button Controls
+			if (digitalRead(BUTTON_SWITCH_1) == LOW ){
 			 ButtonSwitch = false;
-			 print("Button 1 Pushed");
-		 }
+				print("Button 1 Pushed");
+			 }
+			 if (digitalRead(BUTTON_SWITCH_2) == LOW){
+				 ButtonSwitch = true;
+				 print("Button 2 Pushed");
+			 }
 
-		 if (digitalRead(BUTTON_SWITCH_2) == LOW){
-			 ButtonSwitch = true;
-			 print("Button 2 Pushed");
-		 }
 
 		 // Challange #2 - Sonic Ranger
 
